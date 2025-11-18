eb27462ca3ca2fc9182d7b4f5ab8c37fba8a9875
lucassidotti <lucassidotti@gmail.com>
2025-11-18 16:41:06 -0300

    save

--- formulario/app.py
+++ formulario/app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 app=Flask(__name__)
-@app.route('/')
+#Lista de datos, simulamos una DB
+perfiles=[]
+@app.route("/")
 def inicio():
-    return render_template("index.html")
-#Formulario
+    return render_template ("index.html")
 
-@app.route('/enviar',methods=["post"])
-def enviar():
-    nombre=request.form['nombre']
-    edad=request.form['edad']
-    tel=request.form['tel']
-    return f"Hola {nombre}, sus datos fueron enviados"
+@app.route("/agregar", methods=["POST"])
+def agregar():
+    nombre=request.form["nombre"]
+    edad=request.form["edad"]
+    tel=request.form["tel"]
+    perfiles.append({"nombre":nombre,"edad":edad,"tel":tel})
+    return redirect(url_for("verPerfil"))
 
-
-
+@app.route("/recibir")
+def verPerfil():
+    return render_template("recibir.html",perfiles=perfiles)
 
 if __name__==("__main__"):
     app.run(debug=True)
